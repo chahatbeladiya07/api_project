8919d9802bcb0af92ce9f4e9ed699e14ce3872ab
chahatbeladiya07 <chahatbeladiya07@gmail.com>
2023-07-14 17:49:10 +0530

    initial commit

--- lib/Screens/homeScreen/homeScreen.dart
+++ lib/Screens/homeScreen/homeScreen.dart
@@ -107,104 +107,23 @@ class _HomeScreenState extends State<HomeScreen> {
     final tempList=context.watch<ApiProvideClass>().tempList;
     return Padding(
       padding: const EdgeInsets.all(15),
-      child: ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (context, index) {
-          final user=users.elementAt(index);
-          return Stack(
-            children: [
-              InkWell(
-                splashColor: Colors.transparent,
-                hoverColor: Colors.transparent,
-                highlightColor: Colors.transparent,
-                onLongPress: () {
-                  islongpress=true;
-                  if(islongpress=true){
-                    if(tempList[index]==false){
-                      tempList[index]=true;
-                    } else {
-                      tempList[index]=false;
-                    }
-                    if(selectedId.contains(user.id)){
-                      selectedId.removeAt(selectedId.indexOf(user.id));
-                    } else {
-                      selectedId.add(user.id);
-                    }
-                    // print("selected Index : $index => ${selectedId.elementAt(index)}");
-                    if(selectedId.isEmpty){
-                      islongpress=false;
-                    }
-                  }
-                  setState(() {});
-                },
-                onTap: (){
-                  if(islongpress==true){
-                    if(tempList[index]==false){
-                      tempList[index]=true;
-                    } else {
-                      tempList[index]=false;
-                    }
-                    if(selectedId.contains(user.id)){
-                      selectedId.removeAt(selectedId.indexOf(user.id));
-                    } else {
-                      selectedId.add(user.id);
-                    }
-                    // print("selected Index : $index => ${selectedId.elementAt(index)}");
-                    if(selectedId.isEmpty){
-                      islongpress=false;
-                    }
-                  }
-                  setState(() {});
-                },
-                child: Container(
-                    margin: const EdgeInsets.only(bottom: 15),
-                    decoration: BoxDecoration(
-                      color: background,
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: tempList[index]==true? Colors.red: appPrimary,width: selectedId.isNotEmpty? 1.5: 1),
-                    ),
-                    width: double.infinity,
-                    padding: const EdgeInsets.all(10),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: 90,
-                          height: 130,
-                          child: user.image==null || user.image==""? Image.asset(
-                            "assets/images/person.jpg",
-                            fit: BoxFit.cover,
-                          ) : Image.network("${user.image}",fit: BoxFit.fill,),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Flexible(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              IconsTitle(svgPath: 'assets/icons/person.svg',title: user.name),
-                              const SizedBox(height: 5,),
-                              IconsTitle(iconPath: 'assets/images/callPng.png',title: user.mobileNumber),
-                              const SizedBox(height: 5,),
-                              IconsTitle(svgPath: 'assets/icons/email.svg',title: user.email),
-                              const SizedBox(height: 5,),
-                              IconsTitle(iconPath: 'assets/images/age.png',title: "${user.age ?? ""}",height: 21,width: 21,),
-                              const SizedBox(height: 5,),
-                              IconsTitle(svgPath: 'assets/icons/location.svg',title: user.address ?? ""),
-                            ],
-                          ),
-                        ),
-                      ],
-                    )
-                ),
-              ),
-              Positioned(
-                right: 0,
-                child:selectedId.isNotEmpty?
-                IconButton(
-                    onPressed: (){
+      child: RefreshIndicator(
+        onRefresh: () async {
+          await context.read<ApiProvideClass>().apiCall();
+        },
+        child: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            final user=users.elementAt(index);
+            return Stack(
+              children: [
+                InkWell(
+                  splashColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onLongPress: () {
+                    islongpress=true;
+                    if(islongpress=true){
                       if(tempList[index]==false){
                         tempList[index]=true;
                       } else {
@@ -219,63 +138,149 @@ class _HomeScreenState extends State<HomeScreen> {
                       if(selectedId.isEmpty){
                         islongpress=false;
                       }
-                      setState(() {});
-                    },
-                      icon: Icon(tempList[index]==true?
-                        Icons.check_circle_rounded:
-                        Icons.circle_outlined
-                    ),
-                ): PopupMenuButton(
-                  onSelected: (value) async {
-                    int click =value ;
-                    int id= user.id;
-                    if(value==0){
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => CreateUserScreen(
-                              name: user.name,
-                              email: user.email,
-                              number: user.mobileNumber,
-                              age: user.age,
-                              address: user.address,
-                              image: user.image,
-                              id: id,
+                    }
+                    setState(() {});
+                  },
+                  onTap: (){
+                    if(islongpress==true){
+                      if(tempList[index]==false){
+                        tempList[index]=true;
+                      } else {
+                        tempList[index]=false;
+                      }
+                      if(selectedId.contains(user.id)){
+                        selectedId.removeAt(selectedId.indexOf(user.id));
+                      } else {
+                        selectedId.add(user.id);
+                      }
+                      // print("selected Index : $index => ${selectedId.elementAt(index)}");
+                      if(selectedId.isEmpty){
+                        islongpress=false;
+                      }
+                    }
+                    setState(() {});
+                  },
+                  child: Container(
+                      margin: const EdgeInsets.only(bottom: 15),
+                      decoration: BoxDecoration(
+                        color: background,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: tempList[index]==true? Colors.red: appPrimary,width: selectedId.isNotEmpty? 1.5: 1),
+                      ),
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 90,
+                            height: 130,
+                            child: user.image==null || user.image==""? Image.asset(
+                              "assets/images/person.jpg",
+                              fit: BoxFit.cover,
+                            ) : Image.network("${user.image}",fit: BoxFit.fill,),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                IconsTitle(svgPath: 'assets/icons/person.svg',title: user.name),
+                                const SizedBox(height: 5,),
+                                IconsTitle(iconPath: 'assets/images/callPng.png',title: user.mobileNumber),
+                                const SizedBox(height: 5,),
+                                IconsTitle(svgPath: 'assets/icons/email.svg',title: user.email),
+                                const SizedBox(height: 5,),
+                                IconsTitle(iconPath: 'assets/images/age.png',title: "${user.age ?? ""}",height: 21,width: 21,),
+                                const SizedBox(height: 5,),
+                                IconsTitle(svgPath: 'assets/icons/location.svg',title: user.address ?? ""),
+                              ],
                             ),
                           ),
-                      );
-                    } else if(click==1){
-                        var isDeleted =await context.read<ApiProvideClass>().apiDelete(id: id);
-                        if(isDeleted.success == true){
-                          users.removeAt(index);
-                        }
-                      }
-                  },
-                  offset: const Offset(-15, 50),
-                  constraints: const BoxConstraints(
-                      maxWidth: 120
+                        ],
+                      )
                   ),
-                  tooltip: "Delete / Edit",
-                  icon: const Icon(Icons.more_vert,color: appPrimary,),
-                  itemBuilder:(context) {
-                    return buttonList.map((e) {
-                      final int click=buttonList.indexOf(e);
-                      return PopupMenuItem(
-                        value: click,
-                        child: CustomButton(
-                          title: e["title"]!,
-                          svgPath: e["svgPath"]!,
-                          height: click==0 ? 22 : 25,
-                          width: click==0 ? 22 : 25,
-                          color: click==0 ? Colors.green : Colors.red,),
-                      );
-                    },).toList();
-                  },
                 ),
-              ),
-            ],
-          );
-        },
+                Positioned(
+                  right: 0,
+                  child:selectedId.isNotEmpty?
+                  IconButton(
+                      onPressed: (){
+                        if(tempList[index]==false){
+                          tempList[index]=true;
+                        } else {
+                          tempList[index]=false;
+                        }
+                        if(selectedId.contains(user.id)){
+                          selectedId.removeAt(selectedId.indexOf(user.id));
+                        } else {
+                          selectedId.add(user.id);
+                        }
+                        // print("selected Index : $index => ${selectedId.elementAt(index)}");
+                        if(selectedId.isEmpty){
+                          islongpress=false;
+                        }
+                        setState(() {});
+                      },
+                        icon: Icon(tempList[index]==true?
+                          Icons.check_circle_rounded:
+                          Icons.circle_outlined
+                      ),
+                  ): PopupMenuButton(
+                    onSelected: (value) async {
+                      int click =value ;
+                      int id= user.id;
+                      if(value==0){
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => CreateUserScreen(
+                                name: user.name,
+                                email: user.email,
+                                number: user.mobileNumber,
+                                age: user.age,
+                                address: user.address,
+                                image: user.image,
+                                id: id,
+                              ),
+                            ),
+                        );
+                      } else if(click==1){
+                          var isDeleted =await context.read<ApiProvideClass>().apiDelete(id: id);
+                          if(isDeleted.success == true){
+                            users.removeAt(index);
+                          }
+                        }
+                    },
+                    offset: const Offset(-15, 50),
+                    constraints: const BoxConstraints(
+                        maxWidth: 120
+                    ),
+                    tooltip: "Delete / Edit",
+                    icon: const Icon(Icons.more_vert,color: appPrimary,),
+                    itemBuilder:(context) {
+                      return buttonList.map((e) {
+                        final int click=buttonList.indexOf(e);
+                        return PopupMenuItem(
+                          value: click,
+                          child: CustomButton(
+                            title: e["title"]!,
+                            svgPath: e["svgPath"]!,
+                            height: click==0 ? 22 : 25,
+                            width: click==0 ? 22 : 25,
+                            color: click==0 ? Colors.green : Colors.red,),
+                        );
+                      },).toList();
+                    },
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
